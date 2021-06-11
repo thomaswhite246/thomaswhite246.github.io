2cda31c38301f8a923a289dcd08c5d4b13c9e1d8
thomaswhite246 <85212932+thomaswhite246@users.noreply.github.com>
2021-06-10 22:37:25 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("detroit2.wav");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -2; //turn down volume
 
 export var adventureMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
 soundArray.push(adventureMusic);
