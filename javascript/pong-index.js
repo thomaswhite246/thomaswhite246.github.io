db4d103962aba88b7c58e59eabb4e702230f32a8
thomaswhite246 <85212932+thomaswhite246@users.noreply.github.com>
2021-06-11 01:07:34 -0500

    Update pong-index.js

--- javascript/pong-index.js
+++ javascript/pong-index.js
@@ -16,9 +16,9 @@ import {
   adventureMusic,
   villageMusic,
   playDeferredSounds
-} from "./apong-audio.js";
+} from "./pong-audio.js";
 //Defaults for game objects
-import { Game, Ball, Paddle } from "./bpong-classes.js";
+import { Game, Ball, Paddle } from "./pong-classes.js";
 //Game Events
 import {
   GameEvent,
@@ -34,9 +34,9 @@ import {
   muteControl,
   volumeUpControl,
   volumeDownControl
-} from "./bpong-events.js";
+} from "./pong-events.js";
 //Utility functions
-import { clamp, scalerange, randomAdjust, boolToOnOff } from "./bpong-util.js";
+import { clamp, scalerange, randomAdjust, boolToOnOff } from "./pong-util.js";
 //import * as Tone from "../lib/Tone.js";
 
 //GAME OBJECTS///DON'T CHANGE THESE
