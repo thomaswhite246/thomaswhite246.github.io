839829696038a85d5a5084f77dd4f06b7ec0a01d
thomaswhite246 <85212932+thomaswhite246@users.noreply.github.com>
2021-06-15 18:31:35 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("tap-percussive.mp3"); //load sound
+export var wallSound = new soundFile("Detroittap.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("tap-resonant.mp3");
