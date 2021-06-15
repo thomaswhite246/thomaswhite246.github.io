38d66f248b5775fadb2c3bc1c800e85d84067c84
thomaswhite246 <85212932+thomaswhite246@users.noreply.github.com>
2021-06-15 18:53:57 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("Detroitpaddlesound.mp3"); //load sound
+export var wallSound = new soundFile("tap-percussive.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("Detroittap.mp3");
@@ -57,14 +57,13 @@ soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("silence.mp3");
+export var adventureMusic = new soundFile("Detroitpaddlesound.mp3");
 soundArray.push(adventureMusic);
-adventureMusic.player.loop = true;
+adventureMusic.player.loop = false;
 
-export var villageMusic = new soundFile("silence.mp3");
+export var villageMusic = new soundFile("Detroitpaddlesound.mp3");
 soundArray.push(villageMusic);
-villageMusic.player.loop = true;
-villageMusic.player.volume.value = -16;
+villageMusic.player.loop = false;
 
 export var backgroundMusic = new soundFile("Detroit.mp3");
 soundArray.push(backgroundMusic);
